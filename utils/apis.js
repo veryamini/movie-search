fff51b18bc2b6f518ef2bc556cdaec80d9f3962d
Yamini Verma <vermayamini13@gmail.com>
2020-05-02 00:28:53 +0530

    Updated env variables

--- utils/apis.js
+++ utils/apis.js
@@ -1,8 +1,6 @@
-import ConfigData from './config'
-
 var APIS = {
-    searchApi: `https://www.omdbapi.com/?apikey=${ConfigData.apiKey}&s=SEARCH`,
-    movieApi: `https://www.omdbapi.com/?apikey=${ConfigData.apiKey}&i=imdbID`,
+    searchApi: `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=SEARCH`,
+    movieApi: `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=imdbID`,
 };
 
 export default APIS;
